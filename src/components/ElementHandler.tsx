d33618ae0f34231f9a3f82e37d9861d0d15f5816
Harshit Sandilya <harshitsandilya@gmail.com>
2025-03-12 00:49:56 +0530

    Prefixed visulaisation frames: small fix

--- src/components/ElementHandler.tsx
+++ src/components/ElementHandler.tsx
@@ -2,9 +2,9 @@ import { Element } from '@/types/visualisation';
 import TextFrameRenderer from '@/components/TextFrameRenderer';
 import EquationFrameRenderer from '@/components/EquationFrameRenderer';
 
-export default function ElementHandler({ element }: { element: Element }) {
+export default function ElementHandler({ element, multi }: { element: Element, multi: boolean }) {
   return (
-    <div className='flex h-[100%] w-[100%] border items-center justify-center'>
+    <div className={`flex h-[100%] w-[100%] border items-center ${multi ? element.id % 2 === 0 ? 'justify-start' : 'justify-end' : 'justify-center'}`}>
       {element.type === 'text' ? (
         <TextFrameRenderer textFrame={element.frames[0]} />
       ) : element.type === 'equation' ? (
--- src/components/RowHandler.tsx
+++ src/components/RowHandler.tsx
@@ -3,14 +3,16 @@ import ElementHandler from '@/components/ElementHandler';
 
 export default function RowHandler({ rows, elements }: { rows: number[] | number, elements: Element[] | Element }) {
   return (
-    <div className='flex flex-row h-[100%] w-[100%] border'>
-      {Array.isArray(rows) ? (
-        rows.map((_, index) => (
-          <ElementHandler key={index} element={Array.isArray(elements) ? elements[index] : elements} />
-        ))
-      ) : (
-        <ElementHandler element={Array.isArray(elements) ? elements[0] : elements} />
-      )}
-    </div>
+    Array.isArray(rows) ? (
+      <div className='flex flex-row h-[100%] w-[100%] border items-center'>
+        {rows.map((_, index) => (
+          <ElementHandler key={index} element={Array.isArray(elements) ? elements[index] : elements} multi={true} />
+        ))}
+      </div>
+    ) : (
+      <div className='flex flex-row h-[100%] w-[100%] border'>
+        <ElementHandler element={Array.isArray(elements) ? elements[0] : elements} multi={false} />
+      </div>
+    )
   );
 }
